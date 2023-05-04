df8be9e08174254d0afe359db4ca03183591736b
Acidus <acidus99@users.noreply.github.com>
2023-05-04 17:46:29 -0400

    easier to create WarcField bodies

---
+++ WarcFields.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace Warc
+{
+	/// <summary>
+	/// Helper class for creating WARC files for warcinfo and metadata records
+	/// </summary>
+	public class WarcFields : List<WarcField>
+	{
+		public const string ContentType = "application/warc-fields";
+
+		public void Add(string name, string value)
+		{
+			Add(new WarcField { Name = name, Value = value });
+		}
+
+		public void Add(string name, object value)
+			=> Add(name, value?.ToString() ?? "");
+
+        public byte[]? ToBytes()
+		{
+            if (Count ==0)
+            {
+                return null;
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (var field in this)
+            {
+                sb.AppendLine($"{field.Name}: {field.Value}");
+            }
+			return Encoding.UTF8.GetBytes(sb.ToString());
+        }
+	}
+
+	public class WarcField
+	{
+		public required string Name { get; set; }
+
+		public required string Value { get; set; }
+	}
+}
+
