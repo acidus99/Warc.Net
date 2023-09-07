3ead3e9c4844091e01ae8a7126bc3c95ff664f40
Acidus <acidus99@users.noreply.github.com>
2023-09-07 18:13:38 -0400

    Create WarcFormatException.cs

---
+++ Parser/WarcFormatException.cs
@@ -0,0 +1,16 @@
+﻿namespace Warc;
+
+	public class WarcFormatException : FormatException
+	{
+    public long RecordOffset { get; private set; }
+
+    public WarcFormatException(long recordOffset, string? message)
+        : base(message)
+    {
+        RecordOffset = recordOffset;
+    }
+
+    public override string ToString()
+        => $"Warc Format Exception. Offset {RecordOffset}. {Message}";
+}
+
